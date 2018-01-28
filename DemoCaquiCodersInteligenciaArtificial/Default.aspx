125aa3d4afcc26cb20af5154a614543d0998d93c
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2018-01-28 13:30:07 -0200

    Commit

--- DemoCaquiCodersInteligenciaArtificial/Default.aspx
+++ DemoCaquiCodersInteligenciaArtificial/Default.aspx
@@ -1,42 +1,150 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DemoCaquiCodersInteligenciaArtificial._Default" %>
+﻿<%@ Page Title="Home Page" Async="true" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DemoCaquiCodersInteligenciaArtificial._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="row text-center">
+                    <div class="col-lg-6">
+                         <asp:FileUpload ID="fuFoto" runat="server" CssClass="btn-file" /> 
+                    </div>
+                     <div class="col-lg-6">
+                        <asp:Button ID="btnUpload" runat="server" Text="Upload"  OnClick="btnUpload_Click"/>
+                     </div>
+                </div>
+               <br />
+                <img src="" id="imgFoto" runat="server" style="width:100%; height:auto;" />
+                <br /><br />
+                <asp:Button ID="btnReconhecimentoFacial" Visible="false" runat="server" Text="Reconhecimento Facial" OnClick="btnReconhecimentoFacial_Click" />
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
+                
+                
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-5 text-center" style="overflow:scroll; max-height:600px;" id="divCaracteristicas" runat="server">
+
+                <h3>Características</h3>
+                <br /><br />
+                <asp:Repeater ID="rptFaces" runat="server" OnItemDataBound="rptFaces_ItemDataBound">
+                    <ItemTemplate>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <img src='Content/Fotos/<%# Eval("FaceId").ToString() + ".jpeg" %>' style="width:100%; height:auto;" />
+                            </div>
+                            <div class="col-lg-6 text-left">
+                                 
+                                <b>Idade:</b> <asp:Label ID="lblIdade" runat="server" Text='<%# Eval("FaceAttributes.Age").ToString() %>'></asp:Label>
+                                <br />
+                                <b>Gênero:</b> <asp:Label ID="lblGenero" runat="server" Text='<%# Eval("FaceAttributes.Gender").ToString() %>'></asp:Label>
+                                 <br />
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="row text-left">
+                            <div class="col-lg-6" >
+                                
+                                <b>Sentimento: </b> 
+                                    <br />
+                                    <div style="padding-left:10px;">
+                                        <ul>
+                                            <li>Raiva: <%# ( Double.Parse(Eval("FaceAttributes.Emotion.Anger").ToString())*100).ToString() %> %</li>
+                                            <li>Desprezo: <%# (Double.Parse( Eval("FaceAttributes.Emotion.Contempt").ToString())*100).ToString()  %> %</li>
+                                            <li>Desgosto: <%# (Double.Parse(Eval("FaceAttributes.Emotion.Disgust").ToString())*100).ToString() %> %</li>
+                                            <li>Medo: <%# (Double.Parse(Eval("FaceAttributes.Emotion.Fear").ToString())*100).ToString() %> %</li>
+                                            <li>Felicidade: <%# (Double.Parse(Eval("FaceAttributes.Emotion.Happiness").ToString())*100).ToString() %> %</li>
+                                             <li>Neutro: <%# (Double.Parse(Eval("FaceAttributes.Emotion.Neutral").ToString())*100).ToString() %> %</li>
+                                             <li>Tristeza: <%# (Double.Parse(Eval("FaceAttributes.Emotion.Sadness").ToString())*100).ToString() %> %</li>
+                                            <li>Surpresa: <%#  (Double.Parse(Eval("FaceAttributes.Emotion.Surprise").ToString())*100).ToString() %></li>
+                                        </ul>
+                                    </div>
+
+                                    <br />
+                        
+
+                        </div>
+                            <div class="col-lg-6">
+                                <b>Maquiagem: </b> 
+                                    <br />
+                                    <div style="padding-left:10px;">
+                                        <ul>
+                                            <li>Olhos: <%# (Eval("FaceAttributes.Makeup.EyeMakeup").ToString().ToLower() == "true"? "Sim":"Não") %></li>
+                                            
+                                        </ul>
+                                    </div>
+                                <br />
+                                 <b>Careca:</b> <%# (Double.Parse(Eval("FaceAttributes.Hair.Bald").ToString())*100).ToString() %> % 
+                                <br />
+                                 <b>Cabelo encoberto:</b> <%# (Eval("FaceAttributes.Hair.Invisible").ToString().ToLower() == "true"? "Sim":"Não") %>
+                                <br />
+                                <b>Sorriso:</b> <%# (Double.Parse(Eval("FaceAttributes.Smile").ToString())*100).ToString() %> %
+                                
+                            </div>
+                        </div>
+                        <hr />
+                   </ItemTemplate>
+                </asp:Repeater>
+
+            </div>
         </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
+        <br /><br />
+        <div id="divEstatistica" runat="server">
+            <div class="row">
+            <div class="col-lg-1">
+                <img src="Content/icone-pessoas.jpg" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblQuantidade" runat="server" Font-Bold="true"></asp:Label>
+            </div>
+            <div class="col-lg-1">
+                <img src="Content/icon-man.png" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblHomens" runat="server" Font-Bold="true"></asp:Label>
+            </div>
+            <div class="col-lg-1">
+                <img src="Content/icon-woman.jpg" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblMulheres" runat="server" Font-Bold="true"></asp:Label>
+            </div>
+                <div class="col-lg-1">
+                <img src="Content/icone-aniversario.jpg" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblAniversario" runat="server" Font-Bold="true"></asp:Label>
+            </div>
         </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
+            <br /><br />
+            <div class="row">
+            <div class="col-lg-1">
+                <img src="Content/icone-feliz.png" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblFeliz" runat="server" Font-Bold="true"></asp:Label>
+            </div>
+            <div class="col-lg-1">
+                <img src="Content/icone-triste.png" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblTriste" runat="server" Font-Bold="true"></asp:Label>
+            </div>
+            <div class="col-lg-1">
+                <img src="Content/icone-oculos.jpg" style="max-width:50px; height:auto;" />
+            </div>
+            <div class="col-lg-2 text-left" >
+                <br />
+                <asp:Label ID="lblOculos" runat="server" Font-Bold="true"></asp:Label>
+            </div>
+        </div>
         </div>
     </div>
+    
 
 </asp:Content>
--- DemoCaquiCodersInteligenciaArtificial/Default.aspx.cs
+++ DemoCaquiCodersInteligenciaArtificial/Default.aspx.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +17,271 @@ namespace DemoCaquiCodersInteligenciaArtificial
 {
     public partial class _Default : Page
     {
+        //Chave da Face API que pode ser obtida na sua conta no Portal do Azure
+        private static string minha_chave = "9de2f0b12a49495fbb2622eaedc79eb9";
+
+        //URL do serviço da API que pode ser obtida na sua conta no Portal do Azure
+        private static string url_servico = "https://australiaeast.api.cognitive.microsoft.com/face/v1.0/detect";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!this.IsPostBack)
+            {
+                divCaracteristicas.Visible = false;
+                divEstatistica.Visible = false;
+            }
         }
+
+
+        //Ação de upload da imagem
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            divCaracteristicas.Visible = false;
+            divEstatistica.Visible = false;
+            rptFaces.DataSource = null;
+            rptFaces.DataBind();
+            btnReconhecimentoFacial.Visible = true;
+
+            if (fuFoto.HasFile)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(fuFoto.FileName);
+                    fuFoto.SaveAs(Server.MapPath("~/Content/Fotos/") + filename);
+
+                    imgFoto.Src = "Content/Fotos/" + filename;
+                    
+                }
+                catch (Exception ex)
+                {
+                   
+                }
+            }
+        }
+
+        protected  void btnReconhecimentoFacial_Click(object sender, EventArgs e)
+        {
+            divCaracteristicas.Visible = true;
+            divEstatistica.Visible = true;
+            MakeAnalysisRequest(Server.MapPath(imgFoto.Src), rptFaces, Server.MapPath("~/Content/Fotos"), lblQuantidade, lblHomens, lblMulheres, lblAniversario, lblOculos, lblFeliz, lblTriste);
+        }
+
+        static async void MakeAnalysisRequest(string caminho_fisico_arquivo, Repeater rpt, string diretorio, Label lblQuantidade, Label lblHomens, Label lblMulheres, Label lblAniversario, Label lblOculos, Label lblFeliz, Label lblTriste)
+        {
+            HttpClient client = new HttpClient();
+
+            //Configuração das inforamções de Cabeçalho 
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", minha_chave);
+
+            //Especificações de informações adicionais ao cabeçalho referente à API.
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // Especificação completo da URL do serviço com respectivos parâmetros
+            string uri = url_servico + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Transformando o arquivo físico em bytes para ser enviado ao serviço.
+
+            byte[] bytes_imagem = GetArrayDaImagem(caminho_fisico_arquivo);
+
+            using (ByteArrayContent content = new ByteArrayContent(bytes_imagem))
+            {
+                //Informações adicionais ao cabeçalho sobre o tipo de informações que trafegará.
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Fazendo chamada ao serviço
+                response = await client.PostAsync(uri, content);
+
+                // JSON retornado pela API.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                //Desserializando o JSON, populando um array do tipo, Face. Este array contém todos os rostos identificados 
+                //serviço do Azure
+                Face [] faces = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Face[]>(contentString);
+
+
+                //Populando o controle do tipo Repeater do Asp.Net Webforms
+                rpt.DataSource = faces;
+                rpt.DataBind();
+                              
+
+                //Criando novas imagens, uma para da cada rosto identificado através das coordenadas
+                //presentes no retorno do serviço.
+
+                #region                 RECORTE DE IMAGENS
+
+                Bitmap imagem_rosto = null;
+
+
+                //Percorrendo o array/lista de rostos identificados
+                foreach (Face face in faces)
+                {
+                    //Especificando um nome para a nova imagem através do identificador único do rosto
+                    //retornado pelo serviço
+                    var nome_imagem_rosto = face.FaceId + ".jpeg" as string;
+                    
+                    //Especificando o caminho completo para salvar a nova imagem "recortada"
+                    var caminho_completo_nova_imagem = diretorio + '/' + nome_imagem_rosto as string;
+
+                    //Posição do retângulo referente ao rosto identificado na imagem original
+                    Rectangle rect = new Rectangle(face.FaceRectangle.Left, 
+                                                   face.FaceRectangle.Top,
+                                                   face.FaceRectangle.Width,
+                                                   face.FaceRectangle.Height);
+
+
+                    //Gerando a nova imagem com o rosto recortado a parti da posição do retângulo
+                    imagem_rosto = ((Bitmap)System.Drawing.Image.FromFile(caminho_fisico_arquivo))
+                                .Clone(rect, ((Bitmap)System.Drawing.Image.FromFile(caminho_fisico_arquivo)).PixelFormat);
+
+                    //Salvando a imagem no diretório
+                    imagem_rosto.Save(caminho_completo_nova_imagem, ImageFormat.Jpeg);
+
+                    if (imagem_rosto != null)
+                        ((IDisposable)imagem_rosto).Dispose();
+                }
+
+                #endregion
+
+                #region ESTATISTICAS
+
+                
+                //Quantidade de homens identificados na imagem pelo serviço 
+                int quantidade_homens = faces.Count(f => f.FaceAttributes.Gender == "male");
+
+                //Quantidade de mulheres identificadas na imagem pelo serviço
+                int quantidade_mulheres = faces.Count(f => f.FaceAttributes.Gender == "female");
+
+                //Quantidade de pessoas na imagem identificadas como aprensentando sentimento feliz
+                //O parâmetro para a apresentação foi o fator de 60
+                int quantidade_feliz = faces.Count(f => f.FaceAttributes.Emotion.Happiness*100  > 60);
+
+                //Quantidade de pessoas na imagem identificadas como apresentando sentimento triste
+                //O parâmetro para a apresentação foi o fator de 60
+                int quantidade_triste = faces.Count(f => f.FaceAttributes.Emotion.Sadness*100 > 60);
+
+                //Calculando a média de idade de todas as pessoas identificadas na imagem
+                double media_aniversarios = faces.Average(f => f.FaceAttributes.Age);
+                
+                //Número de pessoas identificadas na imagem que estavam usando óculos
+                int quantidade_oculos = faces.Count(f => f.FaceAttributes.Glasses.ToString().ToLower() != "noglasses");
+
+                lblQuantidade.Text = faces.Length.ToString() + " pessoa(s)";
+               
+
+
+                if (quantidade_homens == 0)
+                {
+                    lblHomens.Text = "nenhum homem";
+                }
+                else
+                {
+                    if (quantidade_homens == 1)
+                    {
+                        lblHomens.Text = "1 homem";
+                    }
+                    else
+                    {
+                        lblHomens.Text = quantidade_homens.ToString() +  " homens";
+                    }
+                }
+
+                if (quantidade_mulheres == 0)
+                {
+                    lblMulheres.Text = "nenhuma mulher";
+                }
+                else
+                {
+                    if (quantidade_mulheres == 1)
+                    {
+                        lblMulheres.Text = "1 mulher";
+                    }
+                    else
+                    {
+                        lblMulheres.Text = quantidade_mulheres.ToString() + " mulheres";
+                    }
+                }
+
+                lblAniversario.Text = "A média é de " + media_aniversarios.ToString() + " anos" ;
+
+                //Formatação das informações para melhor exibição
+
+                if (quantidade_feliz == 0)
+                {
+                    lblFeliz.Text = "ninguém feliz";
+                }
+                else
+                {
+                    if (quantidade_feliz == 1)
+                    {
+                        lblFeliz.Text = "1 feliz";
+                    }
+                    else
+                    {
+                        lblFeliz.Text = quantidade_feliz.ToString() + " felizes";
+                    }
+                }
+
+
+                if (quantidade_triste == 0)
+                {
+                    lblTriste.Text = "ninguém triste";
+                }
+                else
+                {
+                    if (quantidade_triste == 1)
+                    {
+                        lblTriste.Text = "1 triste";
+                    }
+                    else
+                    {
+                        lblTriste.Text = quantidade_triste.ToString() + " tristes";
+                    }
+                }
+
+                if (quantidade_oculos == 0)
+                {
+                    lblOculos.Text = "ninguém de óculos ";
+                }
+                else
+                {
+                    if (quantidade_oculos == 1)
+                    {
+                        lblOculos.Text = "1 de óculos";
+                    }
+                    else
+                    {
+                        lblOculos.Text = quantidade_oculos.ToString() + " de óculos";
+                    }
+                }
+
+
+                #endregion
+
+            }
+        }
+
+        static byte[] GetArrayDaImagem(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        protected void rptFaces_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            Label lblGenero = (Label) e.Item.FindControl("lblGenero");
+
+            if (lblGenero.Text.Trim() == "male")
+            {
+                lblGenero.Text = "Masculino";
+            }
+            else
+            {
+                lblGenero.Text = "Feminino";
+            }
+        }
+ 
     }
 }
